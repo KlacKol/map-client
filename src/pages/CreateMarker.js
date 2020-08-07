46dead4d5636145b83eda963b4ce482fad8cb268
KlacKol <cheshtouch@gmail.com>
2020-08-07 18:12:35 +0300

    filter date/ CreateMarker.js page

--- src/pages/CreateMarker.js
+++ src/pages/CreateMarker.js
@@ -1,19 +1,143 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import Button from "@material-ui/core/Button";
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import {generateRandomMarker} from "../services/MapService";
+
+
+const useStyles = makeStyles(theme => ({
+    leftContainer: {
+        marginLeft: 0,
+        paddingLeft: 0
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    generateButton: {
+        width: '50%',
+        height: '15vh'
+    },
+    paperForm: {
+        marginTop: theme.spacing(8),
+    }
+}));
 
 const CreateMarker = () => {
 
+    const classes = useStyles();
+    const [description, setDescription] = useState('');
+    const [lat, setLat] = useState(0);
+    const [lng, setLng] = useState(0);
+    const [date, setDate] = useState(new Date());
+    const formRef = useRef('form');
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+    };
+
+
+
     return (
-        <Button
-        variant="contained"
-        color="default"
-        startIcon={<PlaylistAddIcon />}
-        onClick={generateRandomMarker}
+        <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="stretch"
         >
-            Generate random marker
-        </Button>
+            <Grid item xs={6}>
+                <div className={classes.paper}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        startIcon={<PlaylistAddIcon />}
+                        onClick={generateRandomMarker}
+                        className={classes.generateButton}
+                    >
+                        Generate random marker
+                    </Button>
+                    <div className={classes.paperForm}>
+                        <Typography
+                            component="h1"
+                            variant="h3"
+                            align='center'
+                        >
+                            Create new marker
+                        </Typography>
+                        <ValidatorForm
+                            className={classes.form}
+                            onSubmit={handlerSubmit}
+                            onError={errors => console.log(errors)}
+                            ref={formRef}
+                        >
+                            <TextValidator
+                                label='lat'
+                                name='lat'
+                                value={lat}
+                                onChange={({ target: {value}}) => setLat(value)}
+                                validators={['required', 'minNumber:-90', 'maxNumber:90']}
+                                errorMessages={['this field is required', 'minimum -90 ', 'maximum 90 ']}
+                            />
+
+                            <TextValidator
+                                label='lng'
+                                name='lng'
+                                value={lng}
+                                onChange={({ target: {value}}) => setLng(value)}
+                                validators={['required', 'minNumber:-180', 'maxNumber:180']}
+                                errorMessages={['this field is required', 'minimum -180 ', 'maximum 180 ']}
+                            />
+
+                            <TextValidator
+                            label='description'
+                            name='description'
+                            value={description}
+                            onChange={({ target: {value}}) => setDescription(value)}
+                            validators={['required', 'minStringLength:20', 'maxStringLength:100']}
+                            errorMessages={['this field is required', 'minimum 20 characters', 'maximum 100 characters']}
+                            />
+                            <TextField
+                                id="date"
+                                label="date"
+                                type="date"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Create
+                            </Button>
+                        </ValidatorForm>
+                    </div>
+                </div>
+            </Grid>
+        </Grid>
+
     )
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,42 +7,50 @@ import Slider from '@material-ui/core/Slider';
 import Button from "@material-ui/core/Button";
 import {getFilterMarker} from '../store/actions/map';
 
+const mapFilter = {
+    location: [50.0042617, 36.2034271], date: [1990, 2020],
+};
 
 const Home = () => {
     const dispatch = useDispatch();
     const mapRef = useRef();
-    const [userPosition, setUserPosition] = useState([0, 0]);
-    const [dateValue, setDateValue] = React.useState([1990, 2020]);
+    const [dateValue, setDateValue] = React.useState(mapFilter.date);
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(({coords}) =>  {
-            const currentPosition = [coords.latitude, coords.longitude];
-            setUserPosition(currentPosition)
-        });
-        console.log(mapRef.current.leafletElement.getBounds())
+        // navigator.geolocation.getCurrentPosition(({coords}) =>  {
+        //     const currentPosition = [coords.latitude, coords.longitude];
+        //     setUserPosition(currentPosition)
+        // });
+        handleChangeViewPort();
     }, []);
     const data = useSelector(res => res.map.markers.data, shallowEqual);
 
-    const handleChange = (event, newValue) => {
+    const handleChangeDateSlider = (event, newValue) => {
         setDateValue(newValue);
     };
 
     const handleSubmit = async () => {
+        mapFilter.date = dateValue;
+        await handleChangeViewPort();
+    };
+
+    const handleChangeViewPort = async () => {
         const bounds = mapRef.current.leafletElement.getBounds();
         const data = {
             topLeft: bounds.getNorthWest(),
             bottomRight: bounds.getSouthEast(),
-            date: dateValue
+            date: mapFilter.date
         };
         await dispatch(getFilterMarker(data));
     };
 
-    function valuetext(value) {
-        return `${value}°C`;
-    }
-
     return (
         <>
-            <Map center={userPosition} zoom={10} ref={mapRef}>
+            <Map
+                center={mapFilter.location}
+                zoom={10}
+                ref={mapRef}
+                onViewportChanged={handleChangeViewPort}
+            >
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -62,11 +70,10 @@ const Home = () => {
                     </Typography>
                     <Slider
                         value={dateValue}
-                        onChange={handleChange}
+                        onChange={handleChangeDateSlider}
                         valueLabelDisplay="auto"
                         aria-labelledby="range-slider"
-                        getAriaValueText={valuetext}
-                        max={2020}
+                        max={new Date().getFullYear()}
                     />
                 </div>
                 <Button
